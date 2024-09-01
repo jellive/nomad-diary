6ac89124e75dfd45cd08f8628f50421628919b59
Jell <mystral7@naver.com>
2024-09-01 19:40:44 +0900

    [Realm] add object in home

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,8 @@ import { Colors } from '@/constants/Colors'
 import { useDB } from '@/context/context'
 import { Ionicons } from '@expo/vector-icons'
 import { useNavigation } from 'expo-router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { FlatList } from 'react-native'
 import { Appearance, useColorScheme } from 'react-native'
 import styled, { useTheme } from 'styled-components/native'
 
@@ -12,7 +13,7 @@ const View = styled.View`
   flex: 1;
   padding: 0px 30px;
   padding-top: 100px;
-  background-color: ${Colors[colorScheme ?? 'light'].background};
+  background-color: white;
 `
 const Title = styled.Text`
   color: ${Colors[colorScheme ?? 'light'].text};
@@ -32,16 +33,57 @@ const Btn = styled.TouchableOpacity`
   elevation: 5;
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
 `
+
+const Record = styled.View`
+  background-color: ${Colors[colorScheme ?? 'light'].cardColor};
+  flex-direction: row;
+  align-items: center;
+  padding: 10px 20px;
+  border-radius: 10px;
+`
+
+const Emotion = styled.Text`
+  font-size: 26px;
+  margin-right: 10px;
+`
+
+const Message = styled.Text`
+  font-size: 18px;
+  font-weight: 400;
+`
+
+const Separator = styled.View`
+  height: 10px;
+`
+
 const BtnText = styled.Text``
 
 export default function Index() {
   const colorScheme = useColorScheme()
   const navigation = useNavigation()
   const realm = useDB()
-
+  const [feelings, seFeelings] = useState(realm?.objects('Feeling'))
+  useEffect(() => {
+    // const feelings = realm?.objects('Feeling')
+    // console.log('feelings', feelings)
+    // const happy = feelings?.filtered(`emotion = '🤯'`)
+    // console.log('happy', happy)
+  }, [])
   return (
     <View>
       <Title>Home</Title>
+      <FlatList
+        data={feelings}
+        contentContainerStyle={{ paddingVertical: 10 }}
+        ItemSeparatorComponent={Separator}
+        keyExtractor={feeling => feeling._id + ''}
+        renderItem={({ item }) => (
+          <Record>
+            <Emotion>{item.emotion}</Emotion>
+            <Message>{item.message}</Message>
+          </Record>
+        )}
+      />
       <Btn onPress={() => navigation.navigate({ name: 'write' })}>
         <Ionicons name="add" color="white" size={40} />
       </Btn>
