539e48693a247ed49c37e903c7d0643c6e40d5a5
Jell <mystral7@naver.com>
2024-09-01 12:55:27 +0900

    [Realm] add realm

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,39 +7,65 @@ import {
 import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import 'react-native-reanimated'
+import Realm from 'realm'
 
 import { useColorScheme } from '@/hooks/useColorScheme'
+import { View } from 'react-native'
+
+const FeelingSchema: Realm.ObjectSchema = {
+  name: 'Feeling',
+  properties: {
+    _id: 'int',
+    emotion: 'string',
+    message: 'string'
+  },
+  primaryKey: '_id'
+}
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+  const [realm, setRealm] = useState<Realm | null>(null)
   const colorScheme = useColorScheme()
-  const [loaded] = useFonts({
+  const font = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
   })
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync()
-    }
-  }, [loaded])
-
-  if (!loaded) {
-    return null
+  const loadRealm = async () => {
+    const db = await Realm.open({
+      path: 'nomadDiaryDB',
+      schema: [FeelingSchema]
+    })
+    setRealm(db)
   }
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false, presentation: 'modal' }}>
-        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+  useEffect(() => {
+    loadRealm()
+  }, [])
+  console.log(realm)
 
-        <Stack.Screen name="index" />
-        <Stack.Screen name="write" />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+  const onLayoutRootView = useCallback(async () => {
+    if (realm && font) await SplashScreen.hideAsync()
+  }, [realm, font])
+
+  if (!(realm && font)) {
+    // splash는 맨 밑에.
+    return null
+  }
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      {/* onLayout은 꼭 써줘야 함. */}
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{ headerShown: false, presentation: 'modal' }}>
+          {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+
+          <Stack.Screen name="index" />
+          <Stack.Screen name="write" />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ThemeProvider>
+    </View>
   )
 }
